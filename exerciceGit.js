a4211249a1024138817df4a6476e702fc611939e
Emmanuel Degand <degand.emmanuel@orange.fr>
2018-11-29 16:04:38 +0100

    manipulation git creation branche exerciceGit

---
+++ exerciceGit.js
@@ -0,0 +1,22 @@
+
+// let larg = document.getElementbyId("largeur");
+// let haut = document.getElementbyId("hauteur");
+// let button = document.getElementbyId("valider");
+
+$(function(){
+	
+    function perimetre(l,h){
+        return (l+h)*2;
+    }
+
+    function aire(l,h){
+        return l*h;
+    }
+    
+    $('#calculer').click(function(){
+        let largeur=parseInt($('#largeur').val());
+        let longueur=parseInt($('#longueur').val());
+        $('p').text("Périmètre : " + perimetre(largeur,longueur)+" / Aire : "+aire(largeur,longueur));
+    })
+})
+
