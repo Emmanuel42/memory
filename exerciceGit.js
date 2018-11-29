f21bbc830d526212e06f598a495a9e4d089c5abd
Emmanuel Degand <degand.emmanuel@orange.fr>
2018-11-29 16:55:33 +0100

    branche exerciceGit commit2

--- exerciceGit.js
+++ exerciceGit.js
@@ -1,10 +1,7 @@
 
-// let larg = document.getElementbyId("largeur");
-// let haut = document.getElementbyId("hauteur");
-// let button = document.getElementbyId("valider");
 
 $(function(){
-	
+
     function perimetre(l,h){
         return (l+h)*2;
     }
@@ -20,3 +17,17 @@ $(function(){
     })
 })
 
+
+function conversion (jours, heures, minutes){
+
+	return (jours*24*60*60) + (heures*60*60) + (minutes*60);
+
+}
+
+	$('#conversion').click(function(){
+		let jours = parseInt($('#jours').val());
+		let heures = parseInt($('#heures').val());
+		let minutes = parseInt($('#minutes').val());
+		$('#p2').text("Temps en secondes : " + conversion(jours, heures, minutes));
+
+})
